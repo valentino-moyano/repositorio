453d25cd2b0646fdd63278da26b3fdb7f1b2ee8e
Victoria20072022 <victoriarana.2@gmail.com>
2024-10-11 03:34:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@
     function abriCarrtio() {
         carrito.classList.toggle("visible")
     };
+
+// boton para cambiar tema
+   function toggleDarkMode() {
+    const body = document.body;
+    const button =
+     document.querySelector('.toggle-button');
+
+    if (body.classList.contains('light-mode')){
+        body.classList.remove('light-mode')
+        body.classList.add('dark-mode');
+        button.textContent = 'Cambiar a modo claro'
+    }
+    else{
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+        button.textContent = 'Cambiar a modo oscuro';
+    }
+   }
